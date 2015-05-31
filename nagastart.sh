db2bf6478cd2bdcbda2514cd196538c6df7cadab
apocatarsis <raul-13892@hotmail.com>
2015-05-31 21:49:47 +0200

    Create nagastart.sh

---
+++ nagastart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Only for naga in this moment! but you can easily change it or event manually put the id
+xinput | grep Naga | grep keyboard | cut -d= -f2 | cut -b1-2
+xinput set-int-prop $NAGAID "Device Enabled" 8 0
+naga /dev/input/by-id/usb-Razer_Razer_Naga_Epic-if01-event-kbd 
