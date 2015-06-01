ee97a3abdb120508db7c1c5c84c5b3687711ae53
apocatarsis <raul-13892@hotmail.com>
2015-06-01 14:59:47 +0200

    Update nagastart.sh

--- nagastart.sh
+++ nagastart.sh
@@ -2,4 +2,4 @@
 #Only for naga in this moment! but you can easily change it or event manually put the id
 NAGAID=$(xinput | grep Naga | grep keyboard | cut -d= -f2 | cut -b1-2)
 xinput set-int-prop $NAGAID "Device Enabled" 8 0
-naga /dev/input/by-id/usb-Razer_Razer_Naga_Epic-if01-event-kbd 
+sudo naga /dev/input/by-id/usb-Razer_Razer_Naga_Epic-if01-event-kbd 
