6d7c989d4cf6c5c9dcf649ab058dbdaac374106f
apocatarsis <raul-13892@hotmail.com>
2015-06-01 10:25:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ if [ "$(whoami)" != "root" ]; then
 	echo "You need to be root! Aborting"
 	exit 1
 fi
+command -v xdotool >/dev/null 2>&1 || { echo >&2 "I require xdotool but it's not installed!  Aborting."; exit 1; }
 cd src
 g++ -O3 naga.cpp -o naga
 
@@ -12,11 +13,11 @@ exit 1
 fi
 
 mv naga /usr/local/bin/
-chmod u+s /usr/local/bin/
+sudo chmod u+s /usr/local/bin/naga
 
 cd ..
 cp naga.desktop $HOME/.config/autostart/
 mkdir $HOME/.naga
 cp mapping.txt $HOME/.naga/
 
-nohup bash nagastart.sh &
+nohup bash nagastart.sh & >/dev/null
