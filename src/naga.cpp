fa9b92efc0120a89f3a124949193f42e41e15095
Raul P. Pelaez <raul.perez@uam.es>
2018-01-23 14:56:30 +0100

    Add license

--- src/naga.cpp
+++ src/naga.cpp
@@ -1,9 +1,12 @@
 /*
  * Apocatarsis 2016
- * Released with absolutely no warranty, use with your own responsibility.
- * 
- * This version is still in development
- */
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+*/
 #include <cstdio>
 #include <iostream>
 #include <string>
