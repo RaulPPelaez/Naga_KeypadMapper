a171dad1b9278b93fc98b640f743bce716780a1b
Cam <f-rog@hotmail.com>
2018-10-27 03:40:44 -0400

    Add files via upload

--- nagaXinputStart.sh
+++ nagaXinputStart.sh
@@ -3,13 +3,13 @@ if [[ $EUID -ne 0 && $(ps aux | grep naga | grep debug | grep -c root) -ge '1' ]
 	pkexec --user root naga -killroot
 fi
 
-NAGAID2=$(xinput | grep Naga | grep keyboard | cut -d= -f2 | cut -f1)
+NAGAID2=$(xinput | grep Naga | grep pointer | cut -d= -f2 | cut -f1)
 if [[ `echo $NAGAID2 | wc -w` -eq 2 ]]; then
 	if [[ `xinput get-button-map $(echo $NAGAID2 | awk '{print $1}') | grep 10 | wc -l` -eq 1 ]]; then
-		xinput set-int-prop $(echo $NAGAID2 | awk '{print $1}') "Device Enabled" 8 0
+		xinput set-button-map $(echo $NAGAID2 | awk '{print $1}') 1 2 3 4 5 6 7 11 10 8 9 13 14 15
 	else
-		xinput set-int-prop $(echo $NAGAID2 | awk '{print $2}') "Device Enabled" 8 0
+		xinput set-button-map $(echo $NAGAID2 | awk '{print $2}') 1 2 3 4 5 6 7 11 10 8 9 13 14 15
 	fi
 else
-	xinput set-int-prop $NAGAID2 "Device Enabled" 8 0
+	xinput set-button-map $NAGAID2 1 2 3 4 5 6 7 11 10 8 9 13 14 15
 fi
