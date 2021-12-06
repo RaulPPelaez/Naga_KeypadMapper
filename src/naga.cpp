118930f5b253ff995e15ab14e8d5e48030829a15
Cam <f-rog@hotmail.com>
2021-12-05 20:47:03 -0500

    Added Naga Wireless support thanks to Stibax
    With small optimizations and rewrite.

--- src/naga.cpp
+++ src/naga.cpp
@@ -15,44 +15,51 @@
 #define OFFSET 262
 using namespace std;
 
+
+
 class configKey {
-private:
-const string content;
-const bool internal, onKeyPressed;
-public:
-const bool& IsOnKeyPressed() const {
-	return onKeyPressed;
-}
-const bool& isInternal() const {
-	return internal;
-}
-const void execute(string const& command) const {
-	(void)!(system((content+command).c_str()));
-}
-configKey(string&& tcontent, bool tinternal, bool tonKeyPressed) : content(tcontent), internal(tinternal), onKeyPressed(tonKeyPressed){
-}
+	private:
+	const string content;
+	const bool internal, onKeyPressed;
+	public:
+	const bool& IsOnKeyPressed() const {
+		return onKeyPressed;
+	}
+	const bool& isInternal() const {
+		return internal;
+	}
+	const void execute(string const& command) const {
+		(void)!(system((content+command).c_str()));
+	}
+	configKey(string&& tcontent, bool tinternal, bool tonKeyPressed) : content(tcontent), internal(tinternal), onKeyPressed(tonKeyPressed){
+	}
 };
 
+
 class MacroEvent {
-private:
-const configKey * keyType;
-const string type, content;
-public:
-const configKey * KeyType() const {
-	return keyType;
-}
-const string& Type() const {
-	return type;
-}
-const string& Content() const {
-	return content;
-}
-const void execute() const {
-	keyType->execute(content);
-}
-MacroEvent(configKey * tkeyType, string * ttype, string * tcontent) : keyType(tkeyType), type(*ttype), content(*tcontent){
-}
+	private:
+	const configKey * keyType;
+	const string type, content;
+	public:
+	const configKey * KeyType() const {
+		return keyType;
+	}
+	const string& Type() const {
+		return type;
+	}
+	const string& Content() const {
+		return content;
+	}
+	const void execute() const {
+		keyType->execute(content);
+	}
+	MacroEvent(configKey * tkeyType, string * ttype, string * tcontent) : keyType(tkeyType), type(*ttype), content(*tcontent){
+	}
 };
+
+typedef pair<string, configKey*> stringAndConfigKey;
+typedef vector<MacroEvent *> MacroEventVector;
+typedef pair<const char *,const char *> CharAndChar;
 
 class configSwitchScheduler {
 private:
@@ -78,14 +85,14 @@ class NagaDaemon {
 private:
 const string conf_file = string(getenv("HOME")) + "/.naga/keyMap.txt";
 
-std::map<std::string, configKey*> configKeysMap;
-std::map<std::string, std::map<int, std::vector<MacroEvent *> > > macroEventsKeyMap;
+map<string, configKey*> configKeysMap;
+map<string, map<int, MacroEventVector > > macroEventsKeyMap;
 configSwitchScheduler configSwitcher = configSwitchScheduler();
 
 string currentConfigName;
 struct input_event ev1[64];
 int side_btn_fd, extra_btn_fd, size;
-vector<pair<const char *,const char *> > devices;
+vector<CharAndChar > devices;
 
 void loadConf(string configName) {
 	if(!macroEventsKeyMap.contains(configName)) {
@@ -119,15 +126,15 @@ void loadConf(string configName) {
 
 		for (int readingLine = 1; getline(in, commandContent) && readingLine<configEndLine; readingLine++) {
 			if (readingLine>configLine) {
-				if (commandContent[0] == '#' || commandContent.find_first_not_of(' ') == std::string::npos) continue; //Ignore comments, empty lines
+				if (commandContent[0] == '#' || commandContent.find_first_not_of(' ') == string::npos) continue; //Ignore comments, empty lines
 				pos = commandContent.find('=');
 				string commandType = commandContent.substr(0, pos);//commandType = numbers + command type
 				commandContent.erase(0, pos+1);//commandContent = command content
-				commandType.erase(std::remove(commandType.begin(), commandType.end(), ' '), commandType.end());//Erase spaces inside 1st part of the line
+				commandType.erase(remove(commandType.begin(), commandType.end(), ' '), commandType.end());//Erase spaces inside 1st part of the line
 				pos = commandType.find("-");
 				string buttonNumber = commandType.substr(0, pos);//Isolate button number
 				commandType = commandType.substr(pos + 1);//Isolate command type
-				for(auto& c : commandType)
+				for(char & c : commandType)
 				{
 					c = tolower(c);
 				}
@@ -144,8 +151,9 @@ void loadConf(string configName) {
 						if(commandContent.size()==1) {
 							commandContent = hexChar(commandContent[0]);
 						}
-						macroEventsKeyMap[configName][buttonNumberI].emplace_back(new MacroEvent(configKeysMap["keypressonpress"], &commandType, &commandContent));
-						macroEventsKeyMap[configName][buttonNumberI].emplace_back(new MacroEvent(configKeysMap["keyreleaseonrelease"], &commandType, &commandContent));
+						MacroEventVector * currentMacroEvents = &(macroEventsKeyMap[configName][buttonNumberI]);
+						currentMacroEvents->emplace_back(new MacroEvent(configKeysMap["keypressonpress"], &commandType, &commandContent));
+						currentMacroEvents->emplace_back(new MacroEvent(configKeysMap["keyreleaseonrelease"], &commandType, &commandContent));
 					}else if(commandType=="string" || commandType=="stringrelease") {
 						string commandContent2="";
 						for(long unsigned jj=0; jj<commandContent.size(); jj++) {
@@ -166,13 +174,14 @@ void loadConf(string configName) {
 
 string hexChar(char a){
 	stringstream hexedChar;
-	hexedChar << "0x00" << std::hex << (int)(a);
+	hexedChar << "0x00" << hex << (int)(a);
 	return hexedChar.str();
 }
 
 void run() {
 	fd_set readset;
 	ioctl(side_btn_fd, EVIOCGRAB, 1);// Give application exclusive control over side buttons.
+	input_event * ev11 = &ev1[1];
 	while (1) {
 		if(configSwitcher.isRemapScheduled()) {//remap
 			this->loadConf(configSwitcher.RemapString());//change config for macroEvents[ii]->Content()
@@ -182,28 +191,25 @@ void run() {
 		FD_ZERO(&readset);
 		FD_SET(side_btn_fd, &readset);
 		FD_SET(extra_btn_fd, &readset);
-		int rd1 = select(FD_SETSIZE, &readset, NULL, NULL, NULL);
-		if (rd1 == -1) exit(2);
+		if (select(FD_SETSIZE, &readset, NULL, NULL, NULL) == -1) exit(2);
 		if (FD_ISSET(side_btn_fd, &readset))// Side buttons
 		{
-			rd1 = read(side_btn_fd, ev1, size * 64);
-			if (rd1 == -1) exit(2);
-			if (ev1[0].value != ' ' && ev1[1].type == EV_KEY) {//Key event (press or release)
-				switch (ev1[1].code) {
+			if (read(side_btn_fd, ev1, size * 64) == -1) exit(2);
+			if (ev1[0].value != ' ' && ev11->type == EV_KEY) {//Key event (press or release)
+				switch (ev11->code) {
 				case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10:  case 11:  case 12:  case 13:
-					std::thread actionThread(chooseAction, (ev1[1].value == 1), &macroEventsKeyMap[currentConfigName][ev1[1].code - 1], &configSwitcher);//real key number = ev1[1].code - 1
+					thread actionThread(chooseAction, (ev11->value == 1), &macroEventsKeyMap[currentConfigName][ev11->code - 1], &configSwitcher);//real key number = ev11->code - 1
 					actionThread.detach();
 					break;
 				}
 			}
 		}else if (FD_ISSET(extra_btn_fd, &readset))// Extra buttons
 		{
-			rd1 = read(extra_btn_fd, ev1, size * 64);
-			if (rd1 == -1) exit(2);
-			if (ev1[1].type == 1) {//Only extra buttons
-				switch (ev1[1].code) {
+			if (read(extra_btn_fd, ev1, size * 64) == -1) exit(2);
+			if (ev11->type == 1) {//Only extra buttons
+				switch (ev11->code) {
 				case 275: case 276:
-					std::thread actionThread(chooseAction, (ev1[1].value == 1), &macroEventsKeyMap[currentConfigName][ev1[1].code - OFFSET], &configSwitcher);//real key number = ev1[1].code - OFFSET
+					thread actionThread(chooseAction, (ev11->value == 1), &macroEventsKeyMap[currentConfigName][ev11->code - OFFSET], &configSwitcher);//real key number = ev11->code - OFFSET
 					actionThread.detach();
 					break;
 				}
@@ -212,8 +218,8 @@ void run() {
 	}
 }
 
-static void chooseAction(bool pressed, std::vector<MacroEvent *> * relativeMacroEventsPointer, configSwitchScheduler * congSwitcherPointer) {
-	for(auto & macroEventPointer : (*relativeMacroEventsPointer)) {//run all the events at Key
+static void chooseAction(bool pressed, MacroEventVector * relativeMacroEventsPointer, configSwitchScheduler * congSwitcherPointer) {
+	for(MacroEvent * macroEventPointer : (*relativeMacroEventsPointer)) {//run all the events at Key
 		if(macroEventPointer->KeyType()->IsOnKeyPressed() == pressed) {  //test if key state is matching
 			if((macroEventPointer->KeyType()->isInternal())) {  //INTERNAL COMMANDS
 				if (macroEventPointer->Type() == "sleep" || macroEventPointer->Type() == "sleeprelease") {
@@ -241,36 +247,37 @@ NagaDaemon() {
 	devices.emplace_back("/dev/input/by-id/usb-Razer_Razer_Naga_Hex_V2-if02-event-kbd","/dev/input/by-id/usb-Razer_Razer_Naga_Hex_V2-event-mouse");                                     // NAGA HEX v2
 	devices.emplace_back("/dev/input/by-id/usb-Razer_Razer_Naga_Trinity_00000000001A-if02-event-kbd", "/dev/input/by-id/usb-Razer_Razer_Naga_Trinity_00000000001A-event-mouse");        // NAGA Trinity
 	devices.emplace_back("/dev/input/by-id/usb-Razer_Razer_Naga_Left_Handed_Edition-if02-event-kbd", "/dev/input/by-id/usb-Razer_Razer_Naga_Left_Handed_Edition-event-mouse");          // NAGA Left Handed
+	devices.emplace_back("/dev/input/by-id/usb-Razer_Razer_Naga_Pro_000000000000-if02-event-kbd" , "/dev/input/by-id/usb-Razer_Razer_Naga_Pro_000000000000-event-mouse"); 							// NAGA PRO WIRELESS
 
 	//modulable options list to manage internals inside chooseAction method arg1:COMMAND, arg2:isInternal, arg3:onKeyPressed?
-	configKeysMap.insert(std::pair<std::string, configKey*>("key", NULL));//special one
+	configKeysMap.insert(stringAndConfigKey("key", NULL));//special one
 
-	configKeysMap.insert(std::pair<std::string, configKey*>("chmap", new configKey("", true, true)));//change keymap
-	configKeysMap.insert(std::pair<std::string, configKey*>("chmaprelease", new configKey("", true, false)));
+	configKeysMap.insert(stringAndConfigKey("chmap", new configKey("", true, true)));//change keymap
+	configKeysMap.insert(stringAndConfigKey("chmaprelease", new configKey("", true, false)));
 
-	configKeysMap.insert(std::pair<std::string, configKey*>("sleep", new configKey("", true, true)));
-	configKeysMap.insert(std::pair<std::string, configKey*>("sleeprelease", new configKey("", true, false)));
+	configKeysMap.insert(stringAndConfigKey("sleep", new configKey("", true, true)));
+	configKeysMap.insert(stringAndConfigKey("sleeprelease", new configKey("", true, false)));
 
-	configKeysMap.insert(std::pair<std::string, configKey*>("run", new configKey("setsid ", false, true)));
-	configKeysMap.insert(std::pair<std::string, configKey*>("run2", new configKey("", false, true)));
+	configKeysMap.insert(stringAndConfigKey("run", new configKey("setsid ", false, true)));
+	configKeysMap.insert(stringAndConfigKey("run2", new configKey("", false, true)));
 
-	configKeysMap.insert(std::pair<std::string, configKey*>("runrelease", new configKey("setsid ", false, false)));
-	configKeysMap.insert(std::pair<std::string, configKey*>("runrelease2", new configKey("", false, false)));
+	configKeysMap.insert(stringAndConfigKey("runrelease", new configKey("setsid ", false, false)));
+	configKeysMap.insert(stringAndConfigKey("runrelease2", new configKey("", false, false)));
 
-	configKeysMap.insert(std::pair<std::string, configKey*>("keypressonpress", new configKey("setsid xdotool keydown --window getactivewindow ", false, true)));
-	configKeysMap.insert(std::pair<std::string, configKey*>("keypressonrelease", new configKey("setsid xdotool keydown --window getactivewindow ", false, false)));
+	configKeysMap.insert(stringAndConfigKey("keypressonpress", new configKey("setsid xdotool keydown --window getactivewindow ", false, true)));
+	configKeysMap.insert(stringAndConfigKey("keypressonrelease", new configKey("setsid xdotool keydown --window getactivewindow ", false, false)));
 
-	configKeysMap.insert(std::pair<std::string, configKey*>("keyreleaseonpress", new configKey("setsid xdotool keyup --window getactivewindow ", false, true)));
-	configKeysMap.insert(std::pair<std::string, configKey*>("keyreleaseonrelease", new configKey("setsid xdotool keyup --window getactivewindow ", false, false)));
+	configKeysMap.insert(stringAndConfigKey("keyreleaseonpress", new configKey("setsid xdotool keyup --window getactivewindow ", false, true)));
+	configKeysMap.insert(stringAndConfigKey("keyreleaseonrelease", new configKey("setsid xdotool keyup --window getactivewindow ", false, false)));
 
-	configKeysMap.insert(std::pair<std::string, configKey*>("keyclick", new configKey("setsid xdotool key --window getactivewindow ", false, true)));
-	configKeysMap.insert(std::pair<std::string, configKey*>("keyclickrelease", new configKey("setsid xdotool key --window getactivewindow ", false, false)));
+	configKeysMap.insert(stringAndConfigKey("keyclick", new configKey("setsid xdotool key --window getactivewindow ", false, true)));
+	configKeysMap.insert(stringAndConfigKey("keyclickrelease", new configKey("setsid xdotool key --window getactivewindow ", false, false)));
 
-	configKeysMap.insert(std::pair<std::string, configKey*>("string", new configKey("setsid xdotool key --delay 0 --window getactivewindow", false, true)));
-	configKeysMap.insert(std::pair<std::string, configKey*>("stringrelease", new configKey("setsid xdotool key --delay 0 --window getactivewindow", false, false)));
+	configKeysMap.insert(stringAndConfigKey("string", new configKey("setsid xdotool key --delay 0 --window getactivewindow", false, true)));
+	configKeysMap.insert(stringAndConfigKey("stringrelease", new configKey("setsid xdotool key --delay 0 --window getactivewindow", false, false)));
 
 	size = sizeof(struct input_event);
-	for (auto &device : devices) {//Setup check
+	for (CharAndChar &device : devices) {//Setup check
 		if ((side_btn_fd = open(device.first, O_RDONLY)) != -1 &&  (extra_btn_fd = open(device.second, O_RDONLY)) != -1) {
 			clog << "Reading from: " << device.first << " and " << device.second << endl;
 			break;
@@ -287,12 +294,12 @@ NagaDaemon() {
 
 void stopD() {
 	clog << "Stopping possible naga daemon" << endl;
-	(void)!(system(("kill $(ps aux | grep naga | grep debug | grep -v "+ std::to_string((int)getpid()) +" | awk '{print $2}') > /dev/null 2>&1").c_str()));
+	(void)!(system(("kill $(ps aux | grep naga | grep debug | grep -v "+ to_string((int)getpid()) +" | awk '{print $2}') > /dev/null 2>&1").c_str()));
 	(void)!(system(("/usr/local/bin/killroot.sh")));
 };
 
 void stopDRoot() {
-	(void)!(system(("kill $(ps aux | grep naga | grep debug | grep root | grep -v "+ std::to_string((int)getpid()) +" | awk '{print $2}') > /dev/null 2>&1").c_str()));
+	(void)!(system(("kill $(ps aux | grep naga | grep debug | grep root | grep -v "+ to_string((int)getpid()) +" | awk '{print $2}') > /dev/null 2>&1").c_str()));
 };
 
 //arguments manage
